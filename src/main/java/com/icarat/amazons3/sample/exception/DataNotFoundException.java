dd2a44d9d89ff9f61462b2926c541489fd2c0f0a
renukaradhya <drenukaradhya@manthansystems.com>
2017-06-07 10:54:06 +0530

    Removal of unwanted code

--- src/main/java/com/icarat/amazons3/sample/exception/DataNotFoundException.java
+++ src/main/java/com/icarat/amazons3/sample/exception/DataNotFoundException.java
@@ -1,27 +1,25 @@
 package com.icarat.amazons3.sample.exception;
+
 /**
- * 
+ *
  * @author Icarat1
  *
  */
-public class DataNotFoundException extends Exception{
+public class DataNotFoundException extends Exception {
 
-	public static final String BUCKETS_NOT_FOUND = "Buckets not found";
+  public static final String BUCKETS_NOT_FOUND = "Buckets not found";
 
-	public static final String INVALID_BUCKET_NAME = "Invalid Bucket name";
+  public static final String INVALID_BUCKET_NAME = "Invalid Bucket name";
 
-	public static final String INVALID_FILE_PATH = "Invalid file path";
-	
-	public static final String OJECT_DOES_NOT_EXIST = "Object does not exist";
-	
-	private static final long serialVersionUID = 1L;
+  public static final String INVALID_FILE_PATH = "Invalid file path";
 
-	public DataNotFoundException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
+  public static final String OBJECT_DOES_NOT_EXIST = "Object does not exist";
 
+  private static final long serialVersionUID = 1L;
 
-
+  public DataNotFoundException(String message) {
+    super(message);
+    // TODO Auto-generated constructor stub
+  }
 
 }
